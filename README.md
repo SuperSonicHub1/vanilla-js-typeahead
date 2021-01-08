722e18a2a37333e60b20bcfca6e0fa13071248c5
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-01-08 01:43:57 +0000

    Make this repository understandable through comments, flavor text in the HTML, and the addition of a README.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Vanilla JS Typeahead
+
+A small example displaying typeahead through the [`<datalist>`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist) element and less than twenty lines of vanilla JavaScript.
+
+```js
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+const search_box = document.getElementById("search-bar")
+const datalist = document.getElementById("autocomplete")
+
+async function handleChange(e) {
+    const query = e.target.value
+    const res = await fetch(`/autocomplete?q=${encodeURIComponent(query)}`)
+    body = await res.json()
+    removeAllChildNodes(datalist)
+    for (const {result, subtext} of body) {
+        const option = new Option(subtext, result)
+        datalist.appendChild(option)
+    }
+}
+
+search_box.addEventListener('input', handleChange)
+```
+
+## What?
+I know, right! This is pretty awesome! Simple and clean typeahead without the need for a library like [typeahead.js](https://twitter.github.io/typeahead.js/), as long as you don't need more than two text boxes. If you were super ambitious, you could add images via `background-image`.
+
+The only downside I currently see is that updating the results can appear slow and/or glitchy at times.
+
+Also, if you're wondering where I'm getting my autocomplete results from, they're from Google. I request and clean them up on the backend, and then send the results to the frontend.
+
+## Installation
+With [Poetry](https://python-poetry.org/) installed:
+```bash
+# Clone and cd
+poetry install
+python main.py
+```
--- main.py
+++ main.py
@@ -11,14 +11,19 @@ def index():
 
 @app.route('/autocomplete')
 def autocomplete():
+    """Grabs Google's kooky autocomplete API and reformats it for the front-end."""
+    # Tests for existance of query
     query = request.args.get('q', '')
     if not query:
         return jsonify([])
+
+    # Make request to endpoint
     res = requests.get(
         "https://www.google.com/complete/search",
         params={
             "q": query,
             "cp": len(query),
+            # Have no idea what the rest of these do
             "client": "psy-ab",
             "xssi": "t",
             "gs_ri": "gws-wiz",
@@ -27,18 +32,24 @@ def autocomplete():
             "psi": "UaP3X5P4OJG5ggeD27aoCw.1610064722698",
             "dpr": "1.3513514995574951"
         },
+        # A browser user agent is required to get result subtext
         headers={
             "user-agent": "Mozilla/5.0 (X11; CrOS x86_64 13505.73.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.109 Safari/537.36"
         }
     )
+
+    # Valid JSON is stored on the second line
     body = json.loads(
         res.text.splitlines()[1]
     )
     results = []
-    for item in body[0]:
-        result_text = item[3]["zh"] if len(item) > 3 else Markup(item[0]).striptags()
-        result_subtext = item[3]["zi"] if len(item) > 3 else ''
 
+    # Iterate over results
+    for item in body[0]:
+        # Either get the nice looking version of the result or remove <b> tags from the normal version
+        result_text = item[3]["zh"] if len(item) > 3 else Markup(item[0]).striptags()
+        # Get subtext if exists
+        result_subtext = item[3]["zi"] if len(item) > 3 else ''
         results.append({
             "result": result_text,
             "subtext": result_subtext
